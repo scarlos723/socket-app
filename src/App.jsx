5479e0a4093b4cf70b7dc24972cb1d6bb958f01e
scarlos723 <carlossde58@gmail.com>
2023-10-08 02:46:22 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,62 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { socket } from './socket'
-import ConnectionState from './components/ConnectionState'
-import Events from './components/Events'
-import ConnectionManager from './components/ConnectionManager'
 
 function App () {
   const [message, setMessage] = useState('')
-  const [isConnected, setIsConnected] = useState(socket.connected)
+  const [isConnected, setIsConnected] = useState()
   const [fooEvents, setFooEvents] = useState([])
-
+  const [ws, setWs] = useState(null)
   function handlerSubmit (e) {
     e.preventDefault()
     console.log('semd message', message)
+    if (ws) {
+      ws.send('Hola desde React!')
+      console.log('Mensaje enviado')
+    }
   }
   useEffect(() => {
-    function onConnect () {
+    const websocket = new WebSocket('ws://tu-servidor-websocket.com')
+    websocket.onopen = () => {
       setIsConnected(true)
+      console.log('Conectado a WebSocket')
+    }
+    websocket.onmessage = (event) => {
+      setFooEvents((fooEvents) => [...fooEvents, event.data])
+      console.log('Mensaje recibido:', event.data)
+    }
+    websocket.onerror = (error) => {
+      console.error('Error en WebSocket:', error)
+    }
+    websocket.onclose = (event) => {
+      setFooEvents([])
+      if (event.wasClean) {
+        console.log('Conexión cerrada limpiamente')
+      } else {
+        console.error('Desconexión abrupta')
+      }
     }
 
-    function onDisconnect () {
-      setIsConnected(false)
-    }
+    setWs(websocket)
 
-    function onFooEvent (value) {
-      setFooEvents(previous => [...previous, value])
-    }
-
-    socket.on('connect', onConnect)
-    socket.on('disconnect', onDisconnect)
-    socket.on('foo', onFooEvent)
-
+    // Desconectar al desmontar el componente
     return () => {
-      socket.off('connect', onConnect)
-      socket.off('disconnect', onDisconnect)
-      socket.off('foo', onFooEvent)
+      websocket.close()
+      console.log('WebSocket desconectado')
     }
   }, [])
+
   return (
     <>
       <section>
         <h2>
           Socket app
         </h2>
-      <ConnectionState isConnected={ isConnected } />
+        {
+          isConnected ? <p>Connected</p> : <p>Not connected</p>
+        }
+      {/* <ConnectionState isConnected={ isConnected } />
       <Events events={ fooEvents } />
-      <ConnectionManager />
+      <ConnectionManager /> */}
       <form onSubmit={handlerSubmit} action="">
       <input type="text" placeholder='write message...' onChange={(e) => setMessage(e.target.value)} />
       <button>Send</button>
